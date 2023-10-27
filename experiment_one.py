c4d6adf91572aefd93d9c09bce5a6b12de708838
janedataio <149135724+janedataio@users.noreply.github.com>
2023-10-27 05:12:45 +0000

    initial file

---
+++ experiment_one.py
@@ -0,0 +1,10 @@
+# this program will generate text short story from an image
+# demonstrating Langchain - using huggingface -blip
+#
+# lng2text - image to text model
+
+
+# llm - generate short story
+
+
+# text to speech - generate audio story
